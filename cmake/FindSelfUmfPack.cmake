7cf9175e4818dd20a0e232cc5b3cf388b8e8e232
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-05-31 17:12:20 +0400

    gmres and scripts

---
+++ cmake/FindSelfUmfPack.cmake
@@ -0,0 +1,22 @@
+set(UMFPACK_SOURCE_FOUND 1)
+if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/UMFPACK")
+        set(UMFPACK_SOURCE_FOUND 0)
+	message(STATUS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/UMFPACK not found")
+endif (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/UMFPACK")
+
+if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/AMD")
+        set(UMFPACK_SOURCE_FOUND 0)
+	message(STATUS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/AMD not found")
+endif (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/AMD")
+
+if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/UFconfig")
+        set(UMFPACK_SOURCE_FOUND 0)
+	message(STATUS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/UFConfig not found")
+endif (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${LINAL_HOME}/contrib/umfpack/UFconfig")
+
+if (UMFPACK_SOURCE_FOUND)
+	message(STATUS "UMFPACK sources found, using it!")
+else (UMFPACK_SOURCE_FOUND)
+	message(STATUS "UMFPACK sources not found")
+endif (UMFPACK_SOURCE_FOUND)
+
