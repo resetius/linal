e29086f1caf7d2ffa3d453f75aa8c11c85acecdc
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-08-19 18:03:15 +0400

    load matrix

--- tests/test_sparse.cpp
+++ tests/test_sparse.cpp
@@ -24,133 +24,46 @@ void usage (const char * n)
 	exit (-1);
 }
 
-bool check (float val)
-{
-	return fabs (val) < 1e-5;
-}
-
-bool check (double val)
-{
-	return fabs (val) < 1e-12;
-}
-
-template < typename T >
-bool test_solve (int n, int iters)
-{
-	int i, j = 0;
-	if (n <= 0)     n     = 320000;
-	if (iters <= 0) iters = 1;
-
-	bool ret = true;
-
-	vector < T > b (n);
-	vector < T > x1 (n);
-	vector < T > x2 (n);
-	vector < T > x3 (n);
-	vector < T > v (n);
-	fprintf (stderr, "n=%d, iters=%d\n", n, iters);
-
-	/* матрицу записываем по строкам! */
-	SparseSolver  < T, StoreELL < T , Allocator > , StoreELL < T , Allocator > > M1 (n);
-	init_matrix (M1, n);
-
-#ifdef UMFPACK
-	UmfPackSolver < T, StoreCSR < T , Allocator > > M2 (n);
-	init_matrix (M2, n);
-#endif
-
-#ifdef SUPERLU
-	SuperLUSolver < T, StoreCSR < T , Allocator > > M3 (n);
-	init_matrix (M1, n);
-#endif
-
-	for (i = 0; i < n; ++i)
-	{
-		b[i] = 1;
-	}
-
-	Timer t;
-
-	t.restart();
-	for (int k = 0; k < iters; ++k)
-	{
-		M1.solve (&x1[0], &b[0]);
-	}
-	fprintf (stdout, "gmres solve: %lf\n", t.elapsed() );
-#ifdef UMFPACK
-	t.restart();
-	for (int k = 0; k < iters; ++k)
-	{
-		M2.solve (&x2[0], &b[0]);
-	}
-	fprintf (stdout, "umfpack solve: %lf\n", t.elapsed() );
-#endif
-#ifdef SUPERLU
-	t.restart();
-	for (int k = 0; k < iters; ++k)
-	{
-		M3.solve (&x3[0], &b[0]);
-	}
-	fprintf (stdout, "superlu solve: %lf\n", t.elapsed() );
-#endif
-	T nr;
-#if defined(UMFPACK) && defined(SUPERLU)
-	vec_diff (&v[0], &x2[0], &x3[0], (int) x1.size() );
-	nr = vec_norm2 (&v[0], (int) x2.size() );
-	fprintf (stderr, "%.16le\n", (double) nr);
-	ret &= check (nr);
-#endif
-	vec_diff (&v[0], &x1[0], &x2[0], (int) x1.size() );
-	nr = vec_norm2 (&v[0], (int) x2.size() );
-	fprintf (stderr, "%.16le\n", (double) nr);
-//	ret &= check(nr);
-
-	return ret;
-}
-
 template < typename T >
 struct MatrixLoader
 {
 };
 
-template < typename M, typename V >
-void mult_loop(V & x1, M & M1, V & b, int iters)
+template < typename Store >
+void do_test_mult_sparse(FILE * f, int iters)
 {
-	Timer t;
-	t.restart();
-	for (int k = 0; k < iters; ++k)
-	{
-		M1.mult_vector (&x1[0], &b[0]);
-	}
-	fprintf (stdout, "M1 mult_vector: %lf\n", t.elapsed() );
+	Store store;
+	store.restore(f);
 }
 
-template < typename T >
 bool test_mult_sparse (FILE * f, int iters)
 {
-	int i, j = 0;
-	if (iters <= 0) iters = 10000;
+	char tag[4];
+	int size;
 
-	bool ret = true;
+	fread(tag, 4, 1, f);
+	fread(&size, 4, 1, f);
+	fseek(f, 0, SEEK_SET);
 
-	vector < T > b (n);
-	vector < T > x1 (n);
-	vector < T > x2 (n);
-	vector < T > x3 (n);
-	vector < T > v (n);
-	fprintf (stderr, "n=%d, iters=%d\n", n, iters);
-
-	for (i = 0; i < n; ++i)
-	{
-		b[i] = 1;
+	if (!memcmp(tag, "CSR ", 4)) {
+		if (size == 4) {
+			do_test_mult_sparse < StoreCSR < float > > (f, iters);
+		} else if (size == 8) {
+			do_test_mult_sparse < StoreCSR < double > > (f, iters);
+		} else {
+			throw runtime_error("unknown format\n");
+		}
+	} else if (!memcmp(tag, "ELL ", 4)) {
+		if (size == 4) {
+			do_test_mult_sparse < StoreELL < float > > (f, iters);
+		} else if (size == 8) {
+			do_test_mult_sparse < StoreELL < double > > (f, iters);
+		} else {
+			throw runtime_error("unknown format\n");
+		}
+	} else {
+		throw runtime_error("unknown format\n");
 	}
-
-	/* матрицу записываем по строкам! */
-	SparseSolver  < T, StoreELL < T , Allocator > , StoreELL < T , Allocator > > M1 (n);
-	init_matrix (M1, n);
-	mult_loop(x1, M1, b, iters);
-
-	return ret;
 }
 
 int main (int argc, char * argv[])
@@ -158,6 +71,7 @@ int main (int argc, char * argv[])
 	bool result        = true;
 	bool mult_sparse   = false;
 	bool invert_sparse = false;
+	FILE * f = 0;
 
 	int dim   = 0;
 	int iters = 0;
@@ -201,6 +115,18 @@ int main (int argc, char * argv[])
 
 			iters = atoi (argv[i + 1]);
 		}
+		if (!strcmp (argv[i], "--file"))
+		{
+			if (i == argc - 1)
+			{
+				usage (argv[0]);
+			}
+
+			f = fopen (argv[i + 1], "rb");
+			if (!f) {
+				usage (argv[0]);
+			}
+		}
 		if (!strcmp (argv[i], "--help") || !strcmp (argv[i], "-h") )
 		{
 			usage (argv[0]);
@@ -217,42 +143,18 @@ int main (int argc, char * argv[])
 		Timer t;
 		//burn (1.0);
 		t.restart();
-		if (use_double && has_double)
+
+		if (invert_sparse)
 		{
-			fprintf (stderr, "testing double:\n");
-
-			if (invert_sparse)
-			{
-				t.restart();
-				result &= test_solve < double > (dim, iters);
-				fprintf (stderr, "test_solve < double > (): %lf, %d\n", t.elapsed(), (int) result);
-			}
-
-			if (mult_sparse)
-			{
-				t.restart();
-				result &= test_mult < double > (dim, iters);
-				fprintf (stderr, "test_mult < double > (): %lf, %d\n", t.elapsed(), (int) result);
-			}
+			t.restart();
+			fprintf (stderr, "invert_sparse (): %lf, %d\n", t.elapsed(), (int) result);
 		}
-		else
+
+		if (mult_sparse)
 		{
-			fprintf (stderr, "testing float:\n");
-
-			if (invert_sparse)
-			{
-				t.restart();
-				result &= test_solve < float > (dim, iters);
-				fprintf (stderr, "test_solve < float > (): %lf, %d\n", t.elapsed(), (int) result);
-			}
-
-			if (mult_sparse)
-			{
-				t.restart();
-				result &= test_mult < float > (dim, iters);
-				fprintf (stderr, "test_mult < float > (): %lf, %d\n", t.elapsed(), (int) result);
-			}
-
+			t.restart();
+			result &= test_mult_sparse (f, iters);
+			fprintf (stderr, "test_mult_sparse (): %lf, %d\n", t.elapsed(), (int) result);
 		}
 		fprintf (stderr, "elapsed: %lf\n", t.elapsed() );
 	}
@@ -262,6 +164,7 @@ int main (int argc, char * argv[])
 	}
 
 	linal_shutdown();
+	fclose(f);
 	return (int) (!result);
 }
 
